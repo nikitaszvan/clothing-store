c090bb069334a7da30f83719701de79142e2b6d6
Nikita Van <nikitaszvan@gmail.com>
2023-11-12 21:02:43 -0500

    styling

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeNavbar from "./HomeNavbar.jsx";
+
+function App() {
+    return (
+        <div>
+        <HomeNavbar />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/HomeNavbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomeNavbar() {
+    return (
+        <nav class="navbar">
+            <a href="/">Men's Clothes</a>
+        </nav>
+    )
+}
+
+export default HomeNavbar;
