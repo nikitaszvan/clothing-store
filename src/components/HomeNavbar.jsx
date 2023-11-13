14ed6f6a2ee75110af41f485eb0e9018e4c31a65
Nikita Van <nikitaszvan@gmail.com>
2023-11-12 21:57:54 -0500

    style

--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import TrackChangesIcon from '@mui/icons-material/TrackChanges';
 
 function HomeNavbar() {
     return (
         <nav class="navbar">
-            <a href="/">Men's Clothes</a>
+            <div class="nav-div">
+            <h1 href="/" class="nav-links"><TrackChangesIcon fontSize="large" /></h1>
+            <h1 href="/" class="nav-links">PRODUCTS</h1>
+            <h1 href="/" class="nav-links">BRANDS</h1>
+            <h1 href="/" class="nav-links">SALE</h1>
+            </div>
         </nav>
     )
 }
