d6b26f461c425c42f22b04dd39e267e91a912376
Nikita Van <nikitaszvan@gmail.com>
2023-11-14 17:09:54 -0500

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeNavbar from "./HomeNavbar.jsx";
 import FirstHero from "./FirstHero.jsx";
+import SecondHero from "./SecondHero.jsx";
 
 function App() {
     return (
         <div>
         <HomeNavbar />
         <FirstHero />
+        <SecondHero />
         </div>
     )
 }
---
+++ src/components/SecondHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SecondHero() {
+    return (
+        <div className="second-hero">
+            <img className="second-hero-pic" src="/images/fit_1.png" alt="shoe"/>
+            <img className="second-hero-pic" src="/images/fit_2.png" alt="shoe"/>
+            <img className="second-hero-pic" src="/images/fit_2.png" alt="shoe"/>
+        </div>
+    )
+}
+
+export default SecondHero;
