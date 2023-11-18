e40aa79eb5f1113b3988f1023b00c301197e5c27
Nikita Van <nikitaszvan@gmail.com>
2023-11-17 19:20:35 -0500

    add footer image

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function HomeFooter() {
     return (
         <div className="footer">  
+        <img className="footer-image" src="/images/footer-pic.png" alt="man looking down"/>
         </div>
     )
 }
