9a129865263722e9fc28729691601dfc90762f36
Nikita Van <nikitaszvan@gmail.com>
2023-11-18 23:40:56 -0500

    Update HomeFooter.jsx

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function HomeFooter() {
     return (
         <div className="footer">  
         <img className="footer-image" src="/images/footer-pic.png" alt="man looking down"/>
         <h1>COLLECTIVE</h1>
+        <div>
+            <TwitterIcon />
+            <InstagramIcon />
+            <FacebookIcon />
+        </div>
         </div>
     )
 }
