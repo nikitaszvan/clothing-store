dcc4c4291d04ff404fb207be7af48b96738f1157
Nikita Van <nikitaszvan@gmail.com>
2023-11-18 23:38:10 -0500

    Update HomeFooter.jsx

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -4,6 +4,7 @@ function HomeFooter() {
     return (
         <div className="footer">  
         <img className="footer-image" src="/images/footer-pic.png" alt="man looking down"/>
+        <h1>COLLECTIVE</h1>
         </div>
     )
 }
