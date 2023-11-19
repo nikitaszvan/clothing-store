ade3cb4d3e72ef40bde23d0dba53aa6213156380
Nikita Van <nikitaszvan@gmail.com>
2023-11-18 23:57:12 -0500

    Update FooterTextbox.jsx

---
+++ src/components/FooterTextbox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FooterTextbox() {
+    return (
+        <div>
+            <h2>Section</h2>
+            <p>Contact</p>
+            <p>About Us</p>
+            <p>Reach Out</p>
+        </div>
+    )
+}
+
+export default FooterTextbook;
