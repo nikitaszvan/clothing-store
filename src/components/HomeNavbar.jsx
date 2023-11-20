bb1a0752d62bdb9d82e2ff93570f4d649fd9e626
Nikita Van <nikitaszvan@gmail.com>
2023-11-19 22:57:48 -0500

    Functionality

--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TrackChangesIcon from '@mui/icons-material/TrackChanges';
 
 function HomeNavbar() {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const buttonStyle = {
+        backgroundColor: isHovered ? '#424242' : '#F1EDE4',
+        color: isHovered ? '#F1EDE4' : '#424242'
+      };
+    
     return (
         <div className="navbar">
-            <button href="/" className="nav-links"><TrackChangesIcon fontSize="large" /></button>
-            <button href="/" className="nav-links">PRODUCTS</button>
-            <button href="/" className="nav-links">BRANDS</button>
-            <button href="/" className="nav-links">SALE</button>
-            <button href="/" className="nav-links">ACCESSORIES</button>
-            <button href="/" className="nav-links">LOOKS</button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}
+          ><TrackChangesIcon fontSize="large" /></button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}>PRODUCTS</button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}>BRANDS</button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}>SALE</button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}>ACCESSORIES</button>
+            <button href="/" className="nav-links" onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          style={buttonStyle}>LOOKS</button>
         </div>
     )
 }
